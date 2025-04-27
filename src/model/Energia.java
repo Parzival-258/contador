db742c71f23b9f754e80a5033ecdd68be8f6490c
Parzival-258 <sergio.leonardo2909@gmail.com>
2025-04-26 23:05:01 -0500

    se creo las matrices de los meses al igual que las franjas con sus respectivos valores, ademas de poder modificar cualquier matriz que este dentro de las opciones

---
+++ src/model/Energia.java
@@ -0,0 +1,31 @@
+package model;
+import java.util.Random;
+
+public class Energia {
+
+    private double[][] enero;
+    private double[][] febrero;
+    private double[][] marzo;
+    private double[][] abril;
+    private double[][] mayo;
+    private double[][] junio;
+    private double[][] julio;
+    private double[][] agosto;
+    private double[][] septiembre;
+    private double[][] octubre;
+    private double[][] noviembre;
+    private double[][] diciembre;
+    
+    public Energia(){
+        enero = new double[31][24];
+        febrero = new double[28][24];
+        marzo = new double[31][24];
+        abril = new double[30][24];
+        mayo = new double[31][24];
+        junio = new double[30][24];
+        julio = new double[31][24];
+        agosto = new double[31][24];
+        septiembre = new double[31][24];
+        octubre = new double[][]
+    }
+}
