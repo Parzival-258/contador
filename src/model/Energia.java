44a7a7383e7f75f66f5747833543c8c4ce235b3e
Parzival-258 <sergio.leonardo2909@gmail.com>
2025-04-26 23:33:51 -0500

    se completo todo

--- src/model/Energia.java
+++ src/model/Energia.java
@@ -16,7 +16,7 @@ public class Energia {
     private double[][] noviembre;
     private double[][] diciembre;
     
-    public Energia(){
+    public Energia() {
         enero = new double[31][24];
         febrero = new double[28][24];
         marzo = new double[31][24];
@@ -25,7 +25,63 @@ public class Energia {
         junio = new double[30][24];
         julio = new double[31][24];
         agosto = new double[31][24];
-        septiembre = new double[31][24];
-        octubre = new double[][]
+        septiembre = new double[30][24];
+        octubre = new double[31][24];
+        noviembre = new double[30][24];
+        diciembre = new double[31][24];
+
+        iniciarConsumos();
+
+    }
+    private void iniciarConsumos() {
+        Random rand = new Random();
+        llenarConsumo(enero, rand);
+        llenarConsumo(febrero, rand);
+        llenarConsumo(marzo, rand);
+        llenarConsumo(abril, rand);
+        llenarConsumo(mayo, rand);
+        llenarConsumo(junio, rand);
+        llenarConsumo(julio, rand);
+        llenarConsumo(agosto, rand);
+        llenarConsumo(septiembre, rand);
+        llenarConsumo(octubre, rand);
+        llenarConsumo(noviembre, rand);
+        llenarConsumo(diciembre, rand);
+    }
+    private void llenarConsumo(double[][] mes, Random rand) {
+        for (int dia = 0; dia < mes.length; dia++) {
+            for (int hora = 0; hora < 24; hora++) {
+                if (hora >= 0 && hora <=6) {
+                   mes[dia][hora] = 100 + rand.nextDouble() * 200;
+                } else if (hora >= 7 && hora <= 17) {
+                    mes[dia][hora] = 300 + rand.nextDouble() * 300;
+                }  else if (hora >= 18 && hora <= 23) {
+                    mes[dia][hora] = 600 + rand.nextDouble() * 500;
+                }
+            }
+        }
+    }
+    public double[][] getMes(int mes) {
+        return switch (mes) {
+            case 0 -> enero;
+            case 1 -> febrero;
+            case 2 -> marzo;
+            case 3 -> abril;
+            case 4 -> mayo;
+            case 5 -> junio;
+            case 6 -> julio;
+            case 7 -> agosto;
+            case 8 -> septiembre;
+            case 9 -> octubre;
+            case 10 -> noviembre;
+            case 11 -> diciembre;
+            default -> null;
+        };
+    }
+    public void modificarConsumo(int mes, int dia, int hora, double nuevoConsumo) {
+        double[][] matriz = getMes(mes);
+        if (matriz != null && dia >= 0 && dia < matriz.length && hora >= 0 && hora < 24) {
+            matriz[dia][hora] = nuevoConsumo;
+        }
     }
 }
